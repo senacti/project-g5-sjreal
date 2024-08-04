e3bdc55daa2ec8851116a36ceb1429e237824ced
Pedro20-lab <pedroburgos988@gmail.com>
2024-08-03 22:19:00 -0500

    Finalizado index

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=sjreal;charset=utf8mb4', 'root', '');
-    $result = "Conexion establecida";
-} catch (PDOException $e) {
-    $error = 'Unable to connect to the database server: ' . $e->getMessage() . ' in ' . $e->getFile() . ':' . $e->getLine();
-}
-
-include "result.html.php";
